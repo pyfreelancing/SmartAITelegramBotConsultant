94edfe7aa2451d9aabe763e68433ff3c66a56bd1
pyfreelancing <pyfreelancing@proton.me>
2025-10-13 21:24:39 +0300

    Added /start handler

---
+++ main.py
@@ -0,0 +1,22 @@
+import asyncio
+from aiogram import Bot, Dispatcher
+
+from dotenv import load_dotenv
+import os
+
+from handlers import user_commands
+
+load_dotenv()
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+
+async def main():
+	bot = Bot(token=BOT_TOKEN)
+	dp = Dispatcher()
+	dp.include_router(user_commands.router)
+	await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+	asyncio.run(main())
