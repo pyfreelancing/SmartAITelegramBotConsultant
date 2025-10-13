b023131c029e052388dc11e0c1edb05c92f60ee1
pyfreelancing <pyfreelancing@proton.me>
2025-10-13 22:47:08 +0300

    Added validators into handlers and handlers for the budget

--- handlers/user_messages.py
+++ handlers/user_messages.py
@@ -4,27 +4,53 @@ from aiogram.fsm.context import FSMContext
 import asyncio
 
 from states.search_query import SearchQuery
-from keyboards.inline_keyboards import category_keyboard
+from keyboards.inline_keyboards import category_keyboard, budget_keyboard
+from utils.validators import (
+	is_valid_category_callback, is_valid_category_message,
+	is_valid_budget_callback, is_valid_budget_message
+)
 
 router = Router()
 
 @router.callback_query(
 	SearchQuery.waiting_for_category, 
-	lambda callback: callback.data == "laptops" or \
-		callback.data == "smartphones"
+	lambda callback: is_valid_category_callback(callback)
 	)
 async def process_category_by_button(callback: types.CallbackQuery, state: FSMContext):
-	await callback.message.answer("Введите бюджет:")
+	await callback.message.answer("Введите бюджет:", reply_markup=budget_keyboard())
 	await state.update_data(category=callback.data)
+	await state.set_state(SearchQuery.waiting_for_budget)
 	await callback.answer()
 
 
 @router.message(SearchQuery.waiting_for_category)
 async def process_category_manual(message: types.Message, state: FSMContext):
-	if not ("ноутбук" in message.text.lower() or "смартфон" in message.text.lower()):
+	if not is_valid_category_message(message):
 		await message.answer("Пожалуйста, укажите правильную категорию")
 		return
 	
-	await message.answer("Введите бюджет:")
+	await message.answer("Введите бюджет:", reply_markup=budget_keyboard())
 	await state.update_data(category=message.text)
 	await state.set_state(SearchQuery.waiting_for_budget)
+
+
+@router.callback_query(
+	SearchQuery.waiting_for_budget,
+	lambda callback: is_valid_budget_callback(callback)
+)
+async def process_budget_by_button(callback: types.CallbackQuery, state: FSMContext):
+	await callback.message.answer("Ищу подходящий вариант...")
+	await state.update_data(budget=callback.data)
+	await state.set_state(SearchQuery.waiting_for_results)
+	await callback.answer()
+
+
+@router.message(SearchQuery.waiting_for_budget)
+async def process_budget_manual(message: types.Message, state: FSMContext):
+	if not is_valid_budget_message(message):
+		await message.answer("Пожалуйста, введите бюджет цифрами")
+		return
+	
+	await message.answer("Ищу подходящий вариант...")
+	await state.update_data(budget=message.text)
+	await state.set_state(SearchQuery.waiting_for_results)
