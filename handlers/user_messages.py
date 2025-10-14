176ec382edf7037e3a344fbca72547ad712a7afb
pyfreelancing <pyfreelancing@proton.me>
2025-10-15 00:28:23 +0300

    Added handlers for buttons in the start_keyboard

--- handlers/user_messages.py
+++ handlers/user_messages.py
@@ -1,9 +1,8 @@
 from aiogram import Router, types
-from aiogram.filters import Command, StateFilter
 from aiogram.fsm.context import FSMContext
-import asyncio
 
 from states.search_query import SearchQuery
+from states.free_question import FreeQuestion
 from keyboards.inline_keyboards import category_keyboard, budget_keyboard
 from utils.validators import (
 	is_valid_category_callback, is_valid_category_message,
@@ -11,6 +10,21 @@ from utils.validators import (
 )
 
 router = Router()
+
+
+@router.callback_query(lambda callback: callback.data == "search_query")
+async def start_search(callback: types.CallbackQuery, state: FSMContext):
+	await callback.message.answer("Выберите категорию: ", reply_markup=category_keyboard())
+	await state.set_state(SearchQuery.waiting_for_category)
+	await callback.answer()
+
+
+@router.callback_query(lambda callback: callback.data == "free_question")
+async def start_free_question(callback: types.CallbackQuery, state: FSMContext):
+	await callback.message.answer("Пожалуйста, задайте свой вопрос нашему консультату в свободной форме")
+	await state.set_state(FreeQuestion.waiting_for_question)
+	await callback.answer()
+
 
 @router.callback_query(
 	SearchQuery.waiting_for_category, 
