ec56df28c5939a12f311cca46b56ec9a4e86ed2d
pyfreelancing <pyfreelancing@proton.me>
2025-10-15 15:12:21 +0300

    Added free_question_keyboard and _button to var names into budget_keyboard

--- keyboards/inline_keyboards.py
+++ keyboards/inline_keyboards.py
@@ -23,9 +23,18 @@ def category_keyboard():
 def budget_keyboard():
 	keyboard = InlineKeyboardBuilder()
 	below_20_button = InlineKeyboardButton(text="До 20.000 рулей", callback_data="below 20")
-	from_20_to_60 = InlineKeyboardButton(text="От 20.000 до 60.000 рублей", callback_data="from 20 to 60")
-	from_60_to_100 = InlineKeyboardButton(text="От 60.000 до 100.000 рублей", callback_data="from 60 to 100")
-	above_100 = InlineKeyboardButton(text="Свыше 100.000 рублей", callback_data="above 100")
-	keyboard.add(below_20_button, from_20_to_60, from_60_to_100, above_100)
+	from_20_to_60_button = InlineKeyboardButton(text="От 20.000 до 60.000 рублей", callback_data="from 20 to 60")
+	from_60_to_100_button = InlineKeyboardButton(text="От 60.000 до 100.000 рублей", callback_data="from 60 to 100")
+	above_100_button = InlineKeyboardButton(text="Свыше 100.000 рублей", callback_data="above 100")
+	keyboard.add(below_20_button, from_20_to_60_button, from_60_to_100_button, above_100_button)
 	keyboard.adjust(1, 1, 1, 1)
+	return keyboard.as_markup()
+
+
+def free_question_keyboard():
+	keyboard = InlineKeyboardBuilder()
+	yes_button = InlineKeyboardButton(text="Да, спасибо", callback_data="free_question_yes")
+	no_button = InlineKeyboardButton(text="Нет, задать другой", callback_data="free_question_no")
+	keyboard.add(yes_button, no_button)
+	keyboard.adjust(1, 1)
 	return keyboard.as_markup()
